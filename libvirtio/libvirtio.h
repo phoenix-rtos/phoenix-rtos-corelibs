292e9abae739a1116f5ebc46ebbd94048dc3af77
Lukasz Kosinski <lukasz.kosinski@phoenix-rtos.com>
2020-11-17 18:25:58 +0100

    libvirtio: initial commit with basic virtqueue implementation

---
+++ libvirtio/libvirtio.h
@@ -0,0 +1,229 @@
+/*
+ * Phoenix-RTOS
+ *
+ * VirtIO device core interface
+ *
+ * Copyright 2020 Phoenix Systems
+ * Author: Lukasz Kosinski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#ifndef _LIBVIRTIO_H_
+#define _LIBVIRTIO_H_
+
+#include <endian.h>
+#include <stdint.h>
+
+#include <sys/types.h>
+
+
+typedef struct _virtio_seg_t virtio_seg_t;
+
+
+struct _virtio_seg_t {
+	void *buff;                     /* Buffer exposed to device */
+	unsigned int len;               /* Buffer length */
+	_virtio_seg_t *prev, *next;     /* Doubly linked list */
+};
+
+
+typedef struct {
+	virtio_seg_t *segs;             /* Request segments list */
+	unsigned int rsegs;             /* Number of device readable segments */
+	unsigned int wsegs;             /* Number of device writable segments */
+} virtio_req_t;
+
+
+typedef struct {
+	uint64_t addr;                  /* Buffer physical address */
+	uint32_t len;                   /* Buffer length */
+	uint16_t flags;                 /* Descriptor flags */
+	uint16_t next;                  /* Next chained descriptor index (if flags & 0x1) */
+} __attribute__((packed)) virtio_desc_t;
+
+
+typedef struct {
+	uint16_t flags;                 /* Used buffer notification suppression */
+	uint16_t idx;                   /* Next available request index */
+	uint16_t ring[];                /* Available requests (descriptor chain IDs) */
+} __attribute__((packed)) virtio_avail_t;
+
+
+typedef struct {
+	uint32_t id;                    /* Descriptor chain ID */
+	uint32_t len;                   /* Bytes written into the descriptor chain buffers */
+} __attribute__((packed)) virtio_used_elem_t;
+
+
+typedef struct {
+	uint16_t flags;                 /* Available buffer notification suppression */
+	uint16_t idx;                   /* Next processed request ring index */
+	virtio_used_elem_t ring[];      /* Processed requests */
+} __attribute__((packed)) virtio_used_t;
+
+
+typedef struct {
+	/* Standard split virtqueue layout */
+	volatile virtio_desc_t *desc;   /* Descriptors */
+	volatile virtio_avail_t *avail; /* Avail ring */
+	volatile uint16_t *uevent;      /* Used event notification suppression */
+	volatile virtio_used_t *used;   /* Used ring */
+	volatile uint16_t *aevent;      /* Avail event notification suppression */
+
+	/* Custom helper fields */
+	void **buffs;                   /* Descriptors buffers */
+	void *mem;                      /* Allocated virtqueue memory */
+	unsigned int memsz;             /* Allocated virtqueue memory size */
+	unsigned int idx;               /* Virtqueue index */
+	unsigned int size;              /* Virtqueue size */
+	unsigned int nfree;             /* Number of free descriptors */
+	uint16_t free;                  /* Next free desriptor index */
+	uint16_t last;                  /* Last processed request (trails used ring index) */
+
+	/* Synchronization */
+	handle_t dcond;                 /* Free descriptors condition variable */
+	handle_t dlock;                 /* Free descriptors mutex */
+	handle_t lock;                  /* Virtqueue mutex */
+} virtqueue_t;
+
+
+typedef struct {
+	void* base;                    /* Base registers address */
+	uint64_t features;             /* Device features */
+} virtio_dev_t;
+
+
+/* VirtIO memory barrier */
+static inline void virtio_mb(void)
+{
+	__asm__ __volatile__("" ::: "memory");
+}
+
+
+/* VirtIO device version */
+static inline int virtio_legacy(virtio_dev_t *vdev)
+{
+	return !(vdev->features & 0x100000000ULL);
+}
+
+
+/* VirtIO device to guest endian */
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+	#define virtio_vtog(n) \
+	static inline uint##n##_t virtio_vtog##n##(virtio_dev_t *vdev, uint##n##_t val) \
+	{ \
+		return val; \
+	}
+#elif __BYTE_ORDER == __BIG_ENDIAN
+	#define virtio_vtog(n) \
+	static inline uint##n##_t virtio_vtog##n##(virtio_dev_t *vdev, uint##n##_t val) \
+	{ \
+		if (!virtio_legacy(vdev)) \
+			val = le##n##toh(val); \
+		return val; \
+	}
+#endif
+
+
+virtio_vtog(16)
+virtio_vtog(32)
+virtio_vtog(64)
+
+
+/* Guest to VirtIO device endian */
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+	#define virtio_gtov(n) \
+	static inline uint##n##_t virtio_gtov##n##(virtio_dev_t *vdev, uint##n##_t val) \
+	{ \
+		return val; \
+	}
+#elif __BYTE_ORDER == __BIG_ENDIAN
+	#define virtio_gtov(n) \
+	static inline uint##n##_t virtio_gtov##n##(virtio_dev_t *vdev, uint##n##_t val) \
+	{ \
+		if (!virtio_legacy(vdev)) \
+			val = htole##n##(val); \
+		return val; \
+	}
+#endif
+
+
+virtio_gtov(16)
+virtio_gtov(32)
+virtio_gtov(64)
+
+
+/* Virtqueue data access interface */
+uint8_t virtqueue_read8(virtio_dev_t *vdev, volatile void *addr);
+
+
+uint16_t virtqueue_read16(virtio_dev_t *vdev, volatile void *addr);
+
+
+uint32_t virtqueue_read32(virtio_dev_t *vdev, volatile void *addr);
+
+
+uint64_t virtqueue_read64(virtio_dev_t *vdev, volatile void *addr);
+
+
+void virtqueue_write8(virtio_dev_t *vdev, volatile void *addr, uint8_t val);
+
+
+void virtqueue_write16(virtio_dev_t *vdev, volatile void *addr, uint16_t val);
+
+
+void virtqueue_write32(virtio_dev_t *vdev, volatile void *addr, uint32_t val);
+
+
+void virtqueue_write64(virtio_dev_t *vdev, volatile void *addr, uint64_t val);
+
+
+/* Enables virtqueue interrupts (only a hint for the host) */
+void virtqueue_enableirq(virtio_dev_t *vdev, virtqueue_t *vq);
+
+
+/* Disables virtqueue interrupts (only a hint for the host) */
+void virtqueue_disableirq(virtio_dev_t *vdev, virtqueue_t *vq);
+
+
+/* Allocates virtqueue */
+virtqueue_t *virtqueue_create(unsigned int idx, unsigned int size);
+
+
+/* Releases virtqueue */
+void virtqueue_free(virtqueue_t *vq);
+
+
+/* Enqueues request in avail ring */
+int virtqueue_enqueue(virtio_dev_t *vdev, virtqueue_t *vq, virtio_req_t *req);
+
+
+/* Notifies device of new requests in virtqueue */
+void virtqueue_notify(virtio_dev_t *vdev, virtqueue_t *vq);
+
+
+/* Dequeues request from used ring (returns request head buffer) */
+void *virtqueue_dequeue(virtio_dev_t *vdev, virtqueue_t *vq, unsigned int *len);
+
+
+/* Polls for a processed request (returns request head buffer) */
+void *virtqueue_poll(virtio_dev_t *vdev, virtqueue_t *vq, unsigned int *len);
+
+
+/* VirtIO device interface */
+uint8_t virtio_read8(void *vdev, unsigned int reg);
+uint16_t virtio_read16(void *vdev, unsigned int reg);
+uint32_t virtio_read32(void *vdev, unsigned int reg);
+uint64_t virtio_read64(void *vdev, unsigned int reg);
+
+void virtio_write8(void *vdev, unsigned int reg, uint8_t val);
+void virtio_write16(void *vdev, unsigned int reg, uint16_t val);
+void virtio_write32(void *vdev, unsigned int reg, uint32_t val);
+void virtio_write64(void *vdev, unsigned int reg, uint64_t val);
+
+int virtio_init(void *base, virtio_dev_t *vdev);
+
+#endif
---
+++ libvirtio/virtqueue.c
@@ -0,0 +1,294 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Virtqueue interface
+ *
+ * Copyright 2020 Phoenix Systems
+ * Author: Lukasz Kosinski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/list.h>
+#include <sys/mman.h>
+#include <sys/threads.h>
+
+#include "libvirtio.h"
+
+
+uint8_t virtqueue_read8(virtio_dev_t *vdev, volatile void *addr)
+{
+	return *(volatile uint8_t *)addr;
+}
+
+
+uint16_t virtqueue_read16(virtio_dev_t *vdev, volatile void *addr)
+{
+	return virtio_vtog16(vdev, *(volatile uint16_t *)addr);
+}
+
+
+uint32_t virtqueue_read32(virtio_dev_t *vdev, volatile void *addr)
+{
+	return virtio_vtog32(vdev, *(volatile uint32_t *)addr);
+}
+
+
+uint64_t virtqueue_read64(virtio_dev_t *vdev, volatile void *addr)
+{
+	return virtio_vtog64(vdev, *(volatile uint64_t *)addr);
+}
+
+
+void virtqueue_write8(virtio_dev_t *vdev, volatile void *addr, uint8_t val)
+{
+	*(volatile uint8_t *)addr = val;
+}
+
+
+void virtqueue_write16(virtio_dev_t *vdev, volatile void *addr, uint16_t val)
+{
+	*(volatile uint16_t *)addr = virtio_gtov16(vdev, val);
+}
+
+
+void virtqueue_write32(virtio_dev_t *vdev, volatile void *addr, uint32_t val)
+{
+	*(volatile uint32_t *)addr = virtio_gtov32(vdev, val);
+}
+
+
+void virtqueue_write64(virtio_dev_t *vdev, volatile void *addr, uint64_t val)
+{
+	*(volatile uint64_t *)addr = virtio_gtov32(vdev, val);
+}
+
+
+void virtqueue_enableirq(virtio_dev_t *vdev, virtqueue_t *vq)
+{
+	virtqueue_write16(vdev, &vq->avail->flags, virtqueue_read16(vdev, &vq->avail->flags) & ~0x1);
+	virtio_mb();
+}
+
+
+void virtqueue_disableirq(virtio_dev_t *vdev, virtqueue_t *vq)
+{
+	virtqueue_write16(vdev, &vq->avail->flags, virtqueue_read16(vdev, &vq->avail->flags) | 0x1);
+}
+
+
+virtqueue_t *virtqueue_alloc(unsigned int idx, unsigned int size)
+{
+	unsigned int aoffs = size * sizeof(virtio_desc_t);
+	unsigned int ueoffs = aoffs + sizeof(virtio_avail_t) + size * sizeof(uint16_t);
+	unsigned int uoffs = (ueoffs + sizeof(uint16_t) + _PAGE_SIZE - 1) & ~(_PAGE_SIZE - 1);
+	unsigned int aeoffs = uoffs + sizeof(virtio_used_t) + size * sizeof(virtio_used_elem_t);
+	unsigned int i;
+	virtqueue_t *vq;
+
+	/* Virtqueue index has to be a 2-byte value */
+	if (idx > 0xffff)
+		return NULL;
+
+	/* Virtqueue size has to a be 2-byte power of 2 */
+	if (!size || (size > 0xffff) || ((size - 1) & size))
+		return NULL;
+
+	if ((vq = malloc(sizeof(virtqueue_t))) == NULL)
+		return NULL;
+
+	if ((vq->buffs = malloc(size * sizeof(void *))) == NULL) {
+		free(vq);
+		return NULL;
+	}
+
+	memset(vq->buffs, 0, size * sizeof(void *));
+	vq->memsz = (aeoffs + sizeof(uint16_t) + _PAGE_SIZE - 1) & ~(_PAGE_SIZE - 1);
+
+	/* TODO: allocate contiguous physical memory below 4GB (legacy interface requires 32-bit physical address) */
+	if ((vq->mem = mmap(NULL, vq->memsz, PROT_READ | PROT_WRITE, MAP_UNCACHED | MAP_ANONYMOUS, OID_NULL, 0)) == MAP_FAILED) {
+		free(vq->buffs);
+		free(vq);
+		return NULL;
+	}
+
+	if (condCreate(&vq->dcond) < 0) {
+		munmap(vq->mem, vq->memsz);
+		free(vq->buffs);
+		free(vq);
+		return NULL;
+	}
+
+	if (mutexCreate(&vq->dlock) < 0) {
+		resourceDestroy(vq->dcond);
+		munmap(vq->mem, vq->memsz);
+		free(vq->buffs);
+		free(vq);
+		return NULL;
+	}
+
+	if (mutexCreate(&vq->lock) < 0) {
+		resourceDestroy(vq->dlock);
+		resourceDestroy(vq->dcond);
+		munmap(vq->mem, vq->memsz);
+		free(vq->buffs);
+		free(vq);
+		return NULL;
+	}
+
+	memset(vq->mem, 0, vq->memsz);
+	vq->desc = (virtio_desc_t *)vq->mem;
+	vq->avail = (virtio_avail_t *)((uintptr_t)vq->mem + aoffs);
+	vq->uevent = (uint16_t *)((uintptr_t)vq->mem + ueoffs);
+	vq->used = (virtio_used_t *)((uintptr_t)vq->mem + uoffs);
+	vq->aevent = (uint16_t *)((uintptr_t)vq->mem + aeoffs);
+
+	vq->idx = idx;
+	vq->size = size;
+	vq->nfree = size;
+	vq->free = 0;
+	vq->last = 0;
+
+	for (i = 0; i < size; i++)
+		vq->desc[i].next = i + 1;
+
+	return vq;
+}
+
+
+void virtqueue_free(virtqueue_t *vq)
+{
+	resourceDestroy(vq->lock);
+	resourceDestroy(vq->dlock);
+	resourceDestroy(vq->dcond);
+	munmap(vq->mem, vq->memsz);
+	free(vq->buffs);
+	free(vq);
+}
+
+
+int virtqueue_enqueue(virtio_dev_t *vdev, virtqueue_t *vq, virtio_req_t *req)
+{
+	volatile virtio_desc_t *desc;
+	virtio_seg_t *seg;
+	unsigned int i, n;
+	uint16_t id, idx;
+
+	if ((req == NULL) || (req->segs == NULL))
+		return -EINVAL;
+
+	if ((n = req->rsegs + req->wsegs) < 1)
+		return -EINVAL;
+	else if (n > vq->size)
+		return -ENOSPC;
+
+	mutexLock(vq->dlock);
+
+	/* Wait for n free descriptors */
+	while (vq->nfree < n)
+		condWait(vq->dcond, vq->dlock, 0);
+
+	mutexLock(vq->lock);
+
+	/* Fill out request descriptors */
+	id = vq->free;
+	for (i = 0, seg = req->segs; i < n; i++) {
+		desc = &vq->desc[vq->free];
+		vq->buffs[vq->free] = seg->buff;
+		virtqueue_write64(vdev, &desc->addr, va2pa(seg->buff));
+		virtqueue_write32(vdev, &desc->len, seg->len);
+		virtqueue_write16(vdev, &desc->flags, ((i < n - 1) * 0x1) | ((i >= req->rsegs) * 0x2));
+
+		if (i < n - 1)
+			virtqueue_write16(vdev, &desc->next, desc->next);
+
+		vq->free = desc->next;
+		seg = seg->next;
+	}
+	vq->nfree -= n;
+
+	/* Insert request to avail ring */
+	idx = virtqueue_read16(vdev, &vq->avail->idx);
+	virtqueue_write16(vdev, &vq->avail->ring[idx & (vq->size - 1)], id);
+	virtio_mb();
+
+	/* Update avail index */
+	virtqueue_write16(vdev, &vq->avail->idx, idx + 1);
+
+	mutexUnlock(vq->lock);
+	mutexUnlock(vq->dlock);
+
+	return EOK;
+}
+
+
+void virtqueue_notify(virtio_dev_t *vdev, virtqueue_t *vq)
+{
+	/* Ensure avail index is visible to device */
+	virtio_mb();
+
+	if (!virtqueue_read16(vdev, &vq->avail->flags))
+		virtio_notify(vdev, vq);
+}
+
+
+void *virtqueue_dequeue(virtio_dev_t *vdev, virtqueue_t *vq, unsigned int *len)
+{
+	virtio_used_elem_t *used;
+	uint16_t idx, next;
+	void *buff;
+
+	mutexLock(vq->lock);
+
+	if (vq->last == virtqueue_read16(vdev, &vq->used->idx)) {
+		mutexUnlock(vq->lock);
+		return NULL;
+	}
+
+	/* Get processed request */
+	used = &vq->used->ring[vq->last++ & (vq->size - 1)];
+	virtio_mb();
+
+	/* Get processed request descriptor chain ID and its head buffer */
+	next = virtqueue_read32(vdev, &used->id);
+	buff = vq->buffs[next];
+
+	/* Get number of bytes written do request buffers */
+	if (len != NULL)
+		*len = virtqueue_read32(vdev, &used->len);
+
+	/* Free descriptors */
+	do {
+		idx = next;
+		next = virtqueue_read16(vdev, &vq->desc[idx].next);
+		vq->desc[idx].next = vq->free;
+		vq->buffs[idx] = NULL;
+		vq->free = idx;
+		vq->nfree++;
+	} while (virtqueue_read16(vdev, &vq->desc[idx].flags) & 0x1);
+
+	/* Signal free descriptors */
+	condSignal(vq->dcond);
+
+	mutexUnlock(vq->lock);
+
+	return buff;
+}
+
+
+void *virtqueue_poll(virtio_dev_t *vdev, virtqueue_t *vq, unsigned int *len)
+{
+	void *buff;
+
+	while ((buff = virtqueue_dequeue(vdev, vq, len)) == NULL)
+		usleep(100);
+
+	return buff;
+}
