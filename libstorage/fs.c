e951a45544309b2db504d0d5f3646aa8f299efd0
HBuczynski <hubert.buczynski@phoenix-rtos.com>
2022-04-08 14:03:42 +0200

    libstorage: move filesystem message handler to fs unit
    DTR-102, PD-216

---
+++ libstorage/fs.c
@@ -0,0 +1,163 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Filesystem interface
+ *
+ * Copyright 2022 Phoenix Systems
+ * Author: Hubert Buczynski, Lukasz Kosinski
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include "include/storage/fs.h"
+
+#include <errno.h>
+#include <sys/file.h>
+
+
+void storage_fsHandler(void *data, msg_t *msg)
+{
+	storage_fs_t *fs;
+
+	if (data == NULL) {
+		msg->o.io.err = -EINVAL;
+		return;
+	}
+
+	fs = (storage_fs_t *)data;
+	if (fs->ops == NULL) {
+		msg->o.io.err = -EINVAL;
+		return;
+	}
+
+	switch (msg->type) {
+		case mtOpen:
+			if (fs->ops->open == NULL) {
+				msg->o.io.err = -ENOSYS;
+				break;
+			}
+			fs->ops->open(fs->info, &msg->i.openclose.oid);
+			break;
+
+		case mtClose:
+			if (fs->ops->close == NULL) {
+				msg->o.io.err = -ENOSYS;
+				break;
+			}
+			fs->ops->close(fs->info, &msg->i.openclose.oid);
+			break;
+
+		case mtRead:
+			if (fs->ops->read == NULL) {
+				msg->o.io.err = -ENOSYS;
+				break;
+			}
+			msg->o.io.err = fs->ops->read(fs->info, &msg->i.io.oid, msg->i.io.offs, msg->o.data, msg->o.size);
+			break;
+
+		case mtWrite:
+			if (fs->ops->write == NULL) {
+				msg->o.io.err = -ENOSYS;
+				break;
+			}
+			msg->o.io.err = fs->ops->write(fs->info, &msg->i.io.oid, msg->i.io.offs, msg->i.data, msg->i.size);
+			break;
+
+		case mtTruncate:
+			if (fs->ops->truncate == NULL) {
+				msg->o.io.err = -ENOSYS;
+				break;
+			}
+			msg->o.io.err = fs->ops->truncate(fs->info, &msg->i.io.oid, msg->i.io.len);
+			break;
+
+		case mtDevCtl:
+			msg->o.io.err = -EINVAL;
+			break;
+
+		case mtCreate:
+			if (fs->ops->create == NULL) {
+				msg->o.io.err = -ENOSYS;
+				break;
+			}
+			msg->o.create.err = fs->ops->create(fs->info, &msg->i.create.dir, msg->i.data, &msg->o.create.oid, msg->i.create.mode, msg->i.create.type, &msg->i.create.dev);
+			break;
+
+		case mtDestroy:
+			if (fs->ops->destroy == NULL) {
+				msg->o.io.err = -ENOSYS;
+				break;
+			}
+			msg->o.io.err = fs->ops->destroy(fs->info, &msg->i.destroy.oid);
+			break;
+
+		case mtSetAttr:
+			if (fs->ops->setattr == NULL) {
+				msg->o.io.err = -ENOSYS;
+				break;
+			}
+			msg->o.attr.err = fs->ops->setattr(fs->info, &msg->i.attr.oid, msg->i.attr.type, msg->i.attr.val, msg->i.data, msg->i.size);
+			break;
+
+		case mtGetAttr:
+			if (fs->ops->getattr == NULL) {
+				msg->o.io.err = -ENOSYS;
+				break;
+			}
+			msg->o.attr.err = fs->ops->getattr(fs->info, &msg->i.attr.oid, msg->i.attr.type, &msg->o.attr.val);
+			break;
+
+		case mtLookup:
+			if (fs->ops->lookup == NULL) {
+				msg->o.io.err = -ENOSYS;
+				break;
+			}
+			msg->o.lookup.err = fs->ops->lookup(fs->info, &msg->i.lookup.dir, msg->i.data, &msg->o.lookup.fil, &msg->o.lookup.dev, msg->o.data, msg->o.size);
+			break;
+
+		case mtLink:
+			if (fs->ops->link == NULL) {
+				msg->o.io.err = -ENOSYS;
+				break;
+			}
+			msg->o.io.err = fs->ops->link(fs->info, &msg->i.ln.dir, msg->i.data, &msg->i.ln.oid);
+			break;
+
+		case mtUnlink:
+			if (fs->ops->unlink == NULL) {
+				msg->o.io.err = -ENOSYS;
+				break;
+			}
+			msg->o.io.err = fs->ops->unlink(fs->info, &msg->i.ln.dir, msg->i.data);
+			break;
+
+		case mtReaddir:
+			if (fs->ops->readdir == NULL) {
+				msg->o.io.err = -ENOSYS;
+				break;
+			}
+			msg->o.io.err = fs->ops->readdir(fs->info, &msg->i.readdir.dir, msg->i.readdir.offs, msg->o.data, msg->o.size);
+			break;
+
+		case mtStat:
+			if (fs->ops->statfs == NULL) {
+				msg->o.io.err = -ENOSYS;
+				break;
+			}
+			msg->o.io.err = fs->ops->statfs(fs->info, msg->o.data, msg->o.size);
+			break;
+
+		case mtSync:
+			if (fs->ops->sync == NULL) {
+				msg->o.io.err = -ENOSYS;
+				break;
+			}
+			fs->ops->sync(fs->info, &msg->i.io.oid);
+			break;
+
+		default:
+			break;
+	}
+}
