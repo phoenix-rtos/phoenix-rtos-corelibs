33feb24ea9d62c87a375aea972b91fbff6c18865
Damian Loewnau <damian.loewnau@phoenix-rtos.com>
2022-06-27 19:29:45 +0200

    libuuid: add implementation for the core functions
    Most of the functions implemented in this commit are used in azure-sdk-c, that's why they are added first.
    
    JIRA: PD-289

---
+++ libuuid/uuid.c
@@ -0,0 +1,186 @@
+/*
+ * Phoenix-RTOS
+ *
+ * Universally Unique identifiers library
+ *
+ * Copyright 2022 Phoenix Systems
+ * Author: Damian Loewnau
+ *
+ * This file is part of Phoenix-RTOS.
+ *
+ * %LICENSE%
+ */
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include "include/uuid/uuid.h"
+
+
+static int init = 1;
+
+
+/* Converts 4-bit unsigned char to hex ASCII code */
+static inline char nibble2hex(const unsigned char nibble)
+{
+	return (nibble > 9) ? (nibble & 0xf) - 10 + 'a' : nibble + '0';
+}
+
+
+/* Converts hex ASCII code to 4-bit unsigned char */
+static inline int hex2nibble(unsigned char *nibble, const char hex)
+{
+	*nibble = (hex >= 'a') ? hex - 'a' + 10 : hex - '0';
+	return (*nibble <= 15) ? 0 : -1;
+}
+
+
+static void uuid_useRand(uuid_t out)
+{
+	pid_t pid;
+	struct timespec ts;
+	int i, n;
+
+	/* on some targets clock interval is 10ms, the sleep is to prevent duplication */
+	usleep(10 * 1000);
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	pid = getpid();
+
+	/* only if (pid2 == 255 + n*pid1) and the first 24 bits of tv_sec were the same it could be the same seed */
+	if (init == 1 || (rand() % 2) == 1) {
+		srand((unsigned int)((ts.tv_sec & 0x0FFF) | (pid << 24)));
+		init = 0;
+	}
+
+	/* to prevent same numbers when using the same seed */
+	n = (int)(ts.tv_nsec / 100);
+	for (i = 0; i < n; i++) {
+		rand();
+	}
+
+	for (i = 0; i < (int)sizeof(uuid_t); i++) {
+		out[i] = (unsigned char)(rand() >> (rand() % ((sizeof(int) - 1) * 8)));
+	}
+}
+
+
+void uuid_unparse(const uuid_t uu, char *out)
+{
+	int i;
+	char *temp = out;
+
+	for (i = 0; i < (int)sizeof(uuid_t); i++) {
+		if ((i == 4) || (i == 6) || (i == 8) || (i == 10)) {
+			*temp++ = '-';
+		}
+
+		*(temp++) = nibble2hex(uu[i] >> 4);
+		*(temp++) = nibble2hex(uu[i] & 0xf);
+	}
+
+	*temp = '\0';
+}
+
+
+int uuid_parse(const char *in, uuid_t uu)
+{
+	int i, ret;
+	unsigned char val;
+	const char *temp;
+	size_t len;
+
+	len = strlen(in);
+	if (len != 36) {
+		return -1;
+	}
+
+	temp = in;
+
+	for (i = 0; i < (int)sizeof(uuid_t); i++) {
+		if ((i == 4) || (i == 6) || (i == 8) || (i == 10)) {
+			temp++;
+		}
+
+		ret = hex2nibble(&val, *temp++);
+		uu[i] = val << 4;
+		ret = hex2nibble(&val, *temp++);
+		uu[i] |= val;
+
+		if (ret < 0) {
+			break;
+		}
+	}
+
+	return ret;
+}
+
+
+void uuid_generate_random(uuid_t out)
+{
+	size_t nread, ntoread;
+	const size_t size = sizeof(uuid_t);
+	int useRand;
+	const int readAttempts = 100;
+	int cnt;
+
+	useRand = 0;
+	ntoread = (int)sizeof(uuid_t);
+
+	/* make sure that there is a /dev/random file */
+	FILE *file;
+	file = fopen("/dev/random", "rb");
+	if (file == NULL) {
+		file = fopen("/dev/urandom", "rb");
+		if (file == NULL) {
+			useRand = 1;
+		}
+	}
+
+	if (useRand == 0) {
+		cnt = 0;
+
+		while (ntoread > 0) {
+			nread = fread((void *)&out[size - ntoread], 1, ntoread, file);
+			if (nread == 0 || ferror(file)) {
+				if (cnt++ > readAttempts) {
+					useRand = 1;
+					break;
+				}
+
+				usleep(10 * 1000);
+				continue;
+			}
+			ntoread -= nread;
+		}
+
+		fclose(file);
+	}
+	else if (useRand > 0) {
+		uuid_useRand(out);
+	}
+
+	/* Set uuid variant to 1 - RFC 4122 */
+	out[8] &= 0xDF;
+	out[8] |= 0x40;
+
+	/* Set uuid version to 4 - The randomly/pseuso-randomly generated */
+	out[6] &= 0x0F;
+	out[6] |= 0x40;
+}
+
+
+void uuid_generate(uuid_t out)
+{
+	/* TODO: add fallback to uuid_generate_time when implemented */
+	/* (in case that /dev/random and /dev/urandom aren't available) */
+	uuid_generate_random(out);
+}
+
+
+void uuid_clear(uuid_t uu)
+{
+	memset(uu, 0, sizeof(uuid_t));
+}
