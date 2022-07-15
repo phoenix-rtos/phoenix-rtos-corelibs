e9d82057d7674bee9e1f5343f8b0dec83de6e831
Małgorzata Wróbel <malgorzata.wrobel@phoenix-rtos.com>
2022-07-15 14:02:02 +0200

    libcache: add cache_addToSet and cache_searchInSet functions
    JIRA: RTOS-204

--- libcache/cache.c
+++ libcache/cache.c
@@ -96,3 +96,77 @@ int cache_compareTags(const void *lhs, const void *rhs)
 
 	return ret;
 }
+
+
+/* Adds a line of cache to cache set */
+void cache_addToSet(cacheset_t *cacheSet, cacheline_t *cacheLine)
+{
+	int i = 0;
+	cacheline_t *oldest = NULL, **linePtr = NULL;
+
+	oldest = cacheSet->timestamps[0];
+
+	for (; i < LIBCACHE_NUM_WAYS - 1; ++i) {
+		cacheSet->timestamps[i] = cacheSet->timestamps[i + 1];
+	}
+	cacheSet->timestamps[LIBCACHE_NUM_WAYS - 1] = cacheLine;
+
+	if (oldest == NULL) {
+		for (i = 0; i < LIBCACHE_NUM_WAYS; ++i) {
+			if (cacheSet->lines[i].isValid != '1') {
+				cacheSet->lines[i] = *cacheLine;
+				break;
+			}
+		}
+
+		for (i = 0; i < LIBCACHE_NUM_WAYS; ++i) {
+			if (cacheSet->tags[i] == NULL) {
+				cacheSet->tags[i] = cacheLine;
+				break;
+			}
+		}
+	}
+	else {
+		for (i = 0; i < LIBCACHE_NUM_WAYS; ++i) {
+			if (cacheSet->lines[i].tag == oldest->tag) {
+				cacheSet->lines[i] = *cacheLine;
+				break;
+			}
+		}
+
+		linePtr = bsearch(&oldest, cacheSet->tags, LIBCACHE_NUM_WAYS, sizeof(cacheline_t *), cache_compareTags);
+
+		*linePtr = cacheLine;
+	}
+
+	qsort(cacheSet->tags, LIBCACHE_NUM_WAYS, sizeof(cacheline_t *), cache_compareTags);
+}
+
+
+/* Search a line of cache in a cache set */
+uint32_t *cache_searchInSet(cacheset_t *cacheSet, uint64_t tag)
+{
+	int i = 0, found = 0;
+	uint32_t *ret = NULL;
+	cacheline_t key, *keyPtr = &key, **linePtr = NULL;
+
+	key.tag = tag;
+
+	linePtr = bsearch(&keyPtr, cacheSet->tags, LIBCACHE_NUM_WAYS, sizeof(cacheline_t *), cache_compareTags);
+
+	if (linePtr != NULL) {
+		ret = (*linePtr)->data;
+
+		for (i = 0; i < LIBCACHE_NUM_WAYS - 1; ++i) {
+			if (cacheSet->timestamps[i] == *linePtr) {
+				found = 1;
+			}
+			if (found == 1) {
+				cacheSet->timestamps[i] = cacheSet->timestamps[i + 1];
+			}
+		}
+		cacheSet->timestamps[LIBCACHE_NUM_WAYS - 1] = *linePtr;
+	}
+
+	return ret;
+}
