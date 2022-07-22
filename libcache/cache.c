7561ea8efb261260766da17e13886e7ad8f1aa43
Małgorzata Wróbel <malgorzata.wrobel@phoenix-rtos.com>
2022-07-22 14:13:31 +0200

    libcache: implement cache_init and cache_deinit
    JIRA: RTOS-204

--- libcache/cache.c
+++ libcache/cache.c
@@ -13,177 +13,71 @@
 
 #include "cache.h"
 
-#include <sys/list.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdint.h>
 #include <math.h>
+#include <errno.h>
+
+#define LIBCACHE_NUM_WAYS   4
+#define LIBCACHE_ADDR_WIDTH 64
 
 
-/* Line of cache */
-typedef struct _cacheline_t cacheline_t;
+typedef struct cacheline_s cacheline_t;
 
-struct _cacheline_t {
+
+struct cacheline_s {
 	uint64_t tag;
 	uint32_t *data;
-	unsigned char isValid;
+	unsigned char *isValid;
 	cacheline_t *prev, *next; /* Circular doubly linked list */
 };
 
 
-/* Set of cache */
-typedef struct _cacheset_t {
+typedef struct cacheset_s {
 	cacheline_t *timestamps;
 	cacheline_t **tags;
 	cacheline_t lines[LIBCACHE_NUM_WAYS];
-	int count;
+	size_t count;
 } cacheset_t;
 
 
-/* Cache table */
-struct _cachetable_t {
+struct cachectx_s {
 	cacheset_t *sets;
+
+	size_t numLines;
+	size_t numSets;
+
 	uint64_t tagMask;
 	uint64_t setMask;
 	uint64_t offsetMask;
-	int numSets;
-	int offsetWidth;
+
+	off_t offsetWidth;
+	off_t setBits;
+	off_t tagBits;
+
+	cache_readCb_t readCb;
+	cache_writeCb_t writeCb;
 };
 
 
-/* Frees cache set */
-void cache_freeSet(cacheset_t *cacheSet)
+cacheset_t *cache_setInit(void)
 {
-	free(cacheSet->tags);
+	cacheset_t *set = malloc(sizeof(cacheset_t));
 
-	free(cacheSet);
-}
-
-
-/* Creates cache set */
-cacheset_t *cache_createSet(void)
-{
-	cacheset_t *cacheSet = malloc(sizeof(cacheset_t));
-
-	if (cacheSet == NULL) {
+	if (set == NULL) {
 		return NULL;
 	}
 
-	cacheSet->timestamps = NULL;
-	cacheSet->tags = calloc(LIBCACHE_NUM_WAYS, sizeof(cacheline_t *));
-	memset(cacheSet->lines, 0, sizeof(cacheSet->lines));
+	set->timestamps = NULL;
+	set->tags = calloc(LIBCACHE_NUM_WAYS, sizeof(cacheline_t *));
+	memset(set->lines, 0, sizeof(set->lines));
 
-	if (cacheSet->tags == NULL) {
-		cache_freeSet(cacheSet);
+	if (set->tags == NULL) {
+		free(set);
 		return NULL;
 	}
 
-	return cacheSet;
-}
-
-
-/* Compares values of cache line tags */
-int cache_compareTags(const void *lhs, const void *rhs)
-{
-	int ret = 0;
-
-	cacheline_t **lhsLine = (cacheline_t **)lhs;
-	cacheline_t **rhsLine = (cacheline_t **)rhs;
-
-	if (*lhsLine == NULL && *rhsLine == NULL) {
-		ret = 0;
-	}
-	else if (*lhsLine == NULL && *rhsLine != NULL) {
-		ret = -1;
-	}
-	else if (*lhsLine != NULL && *rhsLine == NULL) {
-		ret = 1;
-	}
-	else {
-		if ((*lhsLine)->tag < (*rhsLine)->tag) {
-			ret = -1;
-		}
-		else if ((*lhsLine)->tag == (*rhsLine)->tag) {
-			ret = 0;
-		}
-		else {
-			ret = 1;
-		}
-	}
-
-	return ret;
-}
-
-
-/* Adds a line of cache to cache set */
-void cache_addToSet(cacheset_t *cacheSet, cacheline_t *cacheLine)
-{
-	int i = 0;
-	cacheline_t **linePtr = NULL, *temp = NULL;
-	cacheline_t *oldest = NULL;
-
-	if (cacheSet->count < LIBCACHE_NUM_WAYS) {
-		for (i = 0; i < LIBCACHE_NUM_WAYS; ++i) {
-			if (cacheSet->lines[i].isValid != '1') {
-				cacheSet->lines[i] = *cacheLine;
-				break;
-			}
-		}
-
-		temp = &(cacheSet->lines[i]);
-
-		for (i = 0; i < LIBCACHE_NUM_WAYS; ++i) {
-			if (cacheSet->tags[i] == NULL) {
-				cacheSet->tags[i] = temp;
-				break;
-			}
-		}
-	}
-	else {
-		oldest = cacheSet->timestamps->prev;
-
-		for (i = 0; i < LIBCACHE_NUM_WAYS; ++i) {
-			if (cacheSet->lines[i].tag == oldest->tag) {
-				cacheSet->lines[i] = *cacheLine;
-				break;
-			}
-		}
-
-		temp = &(cacheSet->lines[i]);
-
-		linePtr = bsearch(&oldest, cacheSet->tags, LIBCACHE_NUM_WAYS, sizeof(cacheline_t *), cache_compareTags);
-
-		*linePtr = cacheLine;
-
-		LIST_REMOVE(&cacheSet->timestamps, oldest);
-		cacheSet->count--;
-	}
-
-	LIST_ADD(&cacheSet->timestamps, temp);
-	cacheSet->count++;
-
-	qsort(cacheSet->tags, LIBCACHE_NUM_WAYS, sizeof(cacheline_t *), cache_compareTags);
-}
-
-
-/* Search a line of cache in a cache set */
-uint32_t *cache_searchInSet(cacheset_t *cacheSet, uint64_t tag)
-{
-	uint32_t *ret = NULL;
-	cacheline_t key, *keyPtr = &key, **linePtr = NULL;
-
-	key.tag = tag;
-
-	linePtr = bsearch(&keyPtr, cacheSet->tags, LIBCACHE_NUM_WAYS, sizeof(cacheline_t *), cache_compareTags);
-
-	if (linePtr != NULL) {
-		ret = (*linePtr)->data;
-
-		LIST_REMOVE(&cacheSet->timestamps, *linePtr);
-		LIST_ADD(&cacheSet->timestamps, *linePtr);
-	}
-
-	return ret;
+	return set;
 }
 
 
@@ -194,40 +88,47 @@ uint64_t cache_genMask(int numBits)
 }
 
 
-cachetable_t *cache_create(void)
+cachectx_t *cache_init(size_t size, size_t lineSize, cache_writeCb_t writeCb, cache_readCb_t readCb)
 {
 	int i = 0, j = 0;
-
-	cachetable_t *cache = malloc(sizeof(cachetable_t));
 	cacheset_t *set = NULL;
+
+	cachectx_t *cache = malloc(sizeof(cachectx_t));
 
 	if (cache == NULL) {
 		return NULL;
 	}
 
-	int numLines = LIBCACHE_MEM_SIZE / LIBCACHE_CACHE_LINE_SIZE;
-	cache->numSets = numLines / LIBCACHE_NUM_WAYS;
+	if (size % lineSize != 0 || size % LIBCACHE_NUM_WAYS != 0) {
+		fprintf(stderr, "Invalid size parameters: size in bytes must be multiple of lineSize and multiple of %d\n", LIBCACHE_NUM_WAYS);
+		free(cache);
+		return NULL;
+	}
+
+	cache->numLines = size / lineSize;
+
+	cache->numSets = cache->numLines / LIBCACHE_NUM_WAYS;
 
 	cache->sets = calloc(cache->numSets, sizeof(cacheset_t));
 
 	if (cache->sets == NULL) {
 		free(cache);
-
 		return NULL;
 	}
 
 	for (; i < cache->numSets; ++i) {
-		set = cache_createSet();
+		set = cache_setInit();
 
 		if (set == NULL) {
 			for (; j < i; ++j) {
 				free(cache->sets[j].tags);
 			}
-
 			free(cache->sets);
 			free(cache);
-		}
 
+			return NULL;
+		}
+		
 		cache->sets[i] = *set;
 
 		cache->sets[i].count = 0;
@@ -235,38 +136,54 @@ cachetable_t *cache_create(void)
 		free(set);
 	}
 
-	cache->offsetWidth = log2(LIBCACHE_CACHE_LINE_SIZE);
-	int setBits = log2(cache->numSets);
-	int tagBits = LIBCACHE_ADDR_WIDTH - setBits - cache->offsetWidth;
-
+	cache->offsetWidth = log2(lineSize);
+	off_t setBits = log2(cache->numSets);
+	off_t tagBits = LIBCACHE_ADDR_WIDTH - setBits - cache->offsetWidth;
 
 	cache->tagMask = cache_genMask(tagBits);
 	cache->setMask = cache_genMask(setBits);
 	cache->offsetMask = cache_genMask(cache->offsetWidth);
 
+	cache->readCb = readCb;
+	cache->writeCb = writeCb;
+
 	return cache;
 }
 
 
-void cache_add(cachetable_t *cache, const uint64_t addr, uint32_t *data)
+int cache_deinit(cachectx_t *cache)
 {
-	uint64_t set = addr >> cache->offsetWidth;
-	set = (set & cache->setMask);
-	/* uint64_t offset = addr & cache->offsetMask; */
-	uint64_t tag = (addr ^ cache->tagMask);
+	int i = 0;
 
-	cacheline_t cacheLine = { tag, data, '1' };
+	for (; i < cache->numSets; ++i) {
+		if (cache->sets != NULL) {
+			free(cache->sets[i].tags);
+		}
+	}
 
-	cache_addToSet(&cache->sets[set], &cacheLine);
+	free(cache->sets);
+
+	free(cache);
+
+	return 0;
 }
 
 
-uint32_t *cache_search(cachetable_t *cache, const uint64_t addr)
+ssize_t cache_read(cachectx_t *cache, const off_t addr, void *buffer)
 {
-	uint64_t set = addr >> cache->offsetWidth;
-	set = (set & cache->setMask);
-	/* uint64_t offset = addr & cache->offsetMask; */
-	uint64_t tag = (addr ^ cache->tagMask);
+}
 
-	return cache_searchInSet(&cache->sets[set], tag);
+
+ssize_t cache_write(cachectx_t *cache, const off_t addr, void *buffer)
+{
+}
+
+
+int cache_flush(cachectx_t *cache, const off_t begAddr, const uint64_t endAddr)
+{
+}
+
+
+int cache_invalidate(cachectx_t *cache, const off_t begAddr, const uint64_t endAddr)
+{
 }
