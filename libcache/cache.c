8735d5669fcb8ed4d45b6dcc698dccb962f99c7a
Małgorzata Wróbel <malgorzata.wrobel@phoenix-rtos.com>
2022-07-29 17:28:06 +0200

    libcache: implement byte reading
    JIRA: RTOS-204

--- libcache/cache.c
+++ libcache/cache.c
@@ -13,13 +13,14 @@
 
 #include "cache.h"
 
-// #include <sys/list.h>
-#include "list.h"
+#include <sys/list.h>
+// #include "list.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <errno.h>
 #include <string.h>
+#include <assert.h>
 
 #define LIBCACHE_NUM_WAYS   4
 #define LIBCACHE_ADDR_WIDTH 64
@@ -32,7 +33,7 @@
 	do { \
 		(f) |= (1 << 0); \
 	} while (0)
-#define CLEAR_VALID \
+#define CLEAR_VALID(f) \
 	do { \
 		(f) &= ~(1 << 0); \
 	} while (0)
@@ -41,7 +42,7 @@
 	do { \
 		(f) |= (1 << 1); \
 	} while (0)
-#define CLEAR_DIRTY \
+#define CLEAR_DIRTY(f) \
 	do { \
 		(f) &= ~(1 << 1); \
 	} while (0)
@@ -52,7 +53,8 @@ typedef struct cacheline_s cacheline_t;
 
 struct cacheline_s {
 	uint64_t tag;
-	uint32_t *data;
+	void *data;
+	uint64_t offset;
 	unsigned char flags;
 	cacheline_t *prev, *next; /* Circular doubly linked list */
 };
@@ -69,6 +71,7 @@ typedef struct cacheset_s {
 struct cachectx_s {
 	cacheset_t *sets;
 
+	size_t lineSize;
 	size_t numLines;
 	size_t numSets;
 
@@ -76,9 +79,9 @@ struct cachectx_s {
 	uint64_t setMask;
 	uint64_t offMask;
 
-	off_t offBitNum;
-	off_t setBitsNum;
-	off_t tagBitsNum;
+	uint64_t offBitNum;
+	uint64_t setBitsNum;
+	uint64_t tagBitsNum;
 
 	cache_readCb_t readCb;
 	cache_writeCb_t writeCb;
@@ -119,7 +122,8 @@ cachectx_t *cache_init(size_t size, size_t lineSize, cache_writeCb_t writeCb, ca
 		return NULL;
 	}
 
-	cache->numLines = size / lineSize;
+	cache->lineSize = lineSize;
+	cache->numLines = size / cache->lineSize;
 
 	cache->numSets = cache->numLines / LIBCACHE_NUM_WAYS;
 
@@ -134,12 +138,12 @@ cachectx_t *cache_init(size_t size, size_t lineSize, cache_writeCb_t writeCb, ca
 		cache_setInit(&cache->sets[i]);
 	}
 
-	cache->offBitNum = log2(lineSize);
-	off_t setBits = log2(cache->numSets);
-	off_t tagBits = LIBCACHE_ADDR_WIDTH - setBits - cache->offBitNum;
+	cache->offBitNum = log2(cache->lineSize);
+	cache->setBitsNum = log2(cache->numSets);
+	cache->tagBitsNum = LIBCACHE_ADDR_WIDTH - cache->setBitsNum - cache->offBitNum;
 
-	cache->tagMask = cache_genMask(tagBits);
-	cache->setMask = cache_genMask(setBits);
+	cache->tagMask = cache_genMask(cache->tagBitsNum);
+	cache->setMask = cache_genMask(cache->setBitsNum);
 	cache->offMask = cache_genMask(cache->offBitNum);
 
 	cache->readCb = readCb;
@@ -151,6 +155,16 @@ cachectx_t *cache_init(size_t size, size_t lineSize, cache_writeCb_t writeCb, ca
 
 int cache_deinit(cachectx_t *cache)
 {
+	int i, j;
+
+	for (i = 0; i < cache->numSets; ++i) {
+		for (j = 0; j < LIBCACHE_NUM_WAYS; ++j) {
+			if (IS_VALID(cache->sets[i].lines[j].flags)) {
+				free(cache->sets[i].lines[j].data);
+				CLEAR_VALID(cache->sets[i].lines[j].flags);
+			}
+		}
+	}
 	free(cache->sets);
 
 	free(cache);
@@ -191,10 +205,10 @@ int cache_compareTags(const void *lhs, const void *rhs)
 }
 
 
-ssize_t cache_writeToSet(cache_writeCb_t writeCb, cacheset_t *set, cacheline_t *line, const off_t offset, int policy)
+ssize_t cache_writeToSet(cache_writeCb_t writeCb, cacheset_t *set, cacheline_t *line, const uint64_t offset, size_t count, int policy)
 {
 	int i, j;
-	ssize_t wrt = 0;
+	ssize_t written = 0;
 	cacheline_t *temp = NULL, **linePtr = NULL;
 
 	if (set->count < LIBCACHE_NUM_WAYS) {
@@ -223,11 +237,12 @@ ssize_t cache_writeToSet(cache_writeCb_t writeCb, cacheset_t *set, cacheline_t *
 
 		/* write-back */
 		if (IS_DIRTY(temp->flags)) {
-			wrt = (*writeCb)(offset, temp->data, sizeof(*temp->data), 1);
+			written = (*writeCb)(offset, temp->data, sizeof(*temp->data), count);
+			free(temp->data);
 		}
 
 		for (i = 0; i < LIBCACHE_NUM_WAYS; ++i) {
-			if (set->lines[i].tag == temp->tag) {
+			if (IS_VALID(set->lines[i].flags) && (set->lines[i].tag == temp->tag)) {
 				set->lines[i] = *line;
 				break;
 			}
@@ -240,10 +255,11 @@ ssize_t cache_writeToSet(cache_writeCb_t writeCb, cacheset_t *set, cacheline_t *
 		case 0:
 			/* write-back */
 			SET_DIRTY(set->lines[i].flags);
+			written = count;
 			break;
 		case 1:
 			/* write-through */
-			wrt = (*writeCb)(offset, temp->data, sizeof(*temp->data), 1);
+			written = (*writeCb)(offset, temp->data, sizeof(*temp->data), count);
 			break;
 		default:
 			break;
@@ -251,13 +267,13 @@ ssize_t cache_writeToSet(cache_writeCb_t writeCb, cacheset_t *set, cacheline_t *
 
 	qsort(set->tags, LIBCACHE_NUM_WAYS, sizeof(cacheline_t *), cache_compareTags);
 
-	return wrt;
+	return written;
 }
 
 
-uint32_t *cache_readFromSet(cacheset_t *set, uint64_t tag, off_t offset)
+void *cache_readFromSet(cacheset_t *set, uint64_t tag, uint64_t offset)
 {
-	uint32_t *ret = NULL;
+	unsigned char *buf = NULL;
 	cacheline_t key, *keyPtr = &key, **linePtr = NULL;
 
 	key.tag = tag;
@@ -265,74 +281,152 @@ uint32_t *cache_readFromSet(cacheset_t *set, uint64_t tag, off_t offset)
 	linePtr = bsearch(&keyPtr, set->tags, LIBCACHE_NUM_WAYS, sizeof(cacheline_t *), cache_compareTags);
 
 	if (linePtr != NULL) {
-		ret = (*linePtr)->data;
+		buf = (unsigned char *)(*linePtr)->data + offset;
 
-		LIST_REMOVE(&set->timestamps, (*linePtr));
+		LIST_REMOVE(&set->timestamps, *linePtr);
 		LIST_ADD(&set->timestamps, *linePtr);
 	}
 
-	return ret;
+	return buf;
 }
 
 
-ssize_t cache_write(cachectx_t *cache, const off_t addr, void *buffer, int policy)
+uint64_t cache_compOffset(const cachectx_t *cache, const uint64_t addr)
+{
+	return addr & cache->offMask;
+}
+
+
+uint64_t cache_compSet(const cachectx_t *cache, const uint64_t addr)
+{
+	return (addr >> cache->offBitNum) & cache->setMask;
+}
+
+
+uint64_t cache_compTag(const cachectx_t *cache, const uint64_t addr)
+{
+	return (addr >> (cache->offBitNum + cache->setBitsNum)) & cache->tagMask;
+}
+
+
+ssize_t cache_write(cachectx_t *cache, const uint64_t addr, void *buffer, size_t count, int policy)
 {
 	ssize_t wrt = 0;
-	unsigned char flags = 0;
-	uint32_t *data = (uint32_t *)buffer;
+	void *dataPtr = NULL;
+	unsigned char *buf = NULL, *dest = NULL, flags = 0;
+	uint64_t index = 0, offset = 0, tag = 0;
+	cacheline_t line;
 
-	off_t setIndex = (addr >> cache->offBitNum) & cache->setMask;
-	off_t offset = addr & cache->offMask;
-	off_t tag = (addr & cache->tagMask);
+	index = cache_compSet(cache, addr);
+	offset = cache_compOffset(cache, addr);
+	tag = cache_compTag(cache, addr);
 
-	printf("set index: %lu\n", setIndex);
-	printf("addr: %lu\n", addr);
-	printf("tag: %lu\n", tag);
+	dataPtr = malloc(sizeof(unsigned char) * cache->lineSize);
+
+	dest = (unsigned char *)dataPtr + offset;
+	/* TODO: make proper writing */
+	if (count > cache->lineSize - offset) {
+		count = cache->lineSize - offset;
+	}
+	memcpy(dest, buffer, count);
 
 	SET_VALID(flags);
 
-	cacheline_t line = { tag, data, flags };
+	line.tag = tag;
+	line.data = dataPtr;
+	line.flags = flags;
+	line.offset = offset;
 
-	return cache_writeToSet(cache->writeCb, &cache->sets[setIndex], &line, offset, policy);
+	return cache_writeToSet(cache->writeCb, &cache->sets[index], &line, offset, count, policy);
 }
 
 
-ssize_t cache_read(cachectx_t *cache, const off_t addr, void *buffer)
+ssize_t cache_readOnMiss(cachectx_t *cache, const uint64_t addr, const uint64_t offset, void *buffer, size_t position, size_t count)
 {
-	uint32_t *data = (uint32_t *)buffer, *ret;
-	ssize_t wrt = 0;
+	ssize_t read = 0, written = 0;
+	void *temp = NULL;
 
-	off_t setIndex = (addr >> cache->offBitNum) & cache->setMask;
-	off_t offset = addr & cache->offMask;
-	off_t tag = (addr & cache->tagMask);
+	temp = calloc(cache->lineSize, sizeof(unsigned char));
 
-	printf("set index: %lu\n", setIndex);
-	printf("offset: %lu\n", offset);
-	printf("tag: %lu\n", tag);
+	read = cache->readCb(addr, temp, sizeof(*temp), cache->lineSize);
 
-	ret = cache_readFromSet(&cache->sets[setIndex], tag, offset);
+	assert(read == cache->lineSize);
 
-	/* cache miss */
-	if (ret == NULL) {
-		cache->readCb(offset, data, sizeof(*data), 1);
-		printf("data read by callback: %u\n", *data);
+	written = cache_write(cache, addr, temp, cache->lineSize, -1);
 
-		wrt = cache_write(cache, addr, data, -1);
+	memcpy(buffer + position, temp + offset, count);
+
+	free(temp);
+
+	return count;
+}
+
+
+/* TODO: add error handling */
+ssize_t cache_read(cachectx_t *cache, uint64_t addr, void *buffer, size_t count)
+{
+	ssize_t position = 0;
+	void *data = NULL;
+	uint64_t index = 0, offset = 0, tag = 0;
+	size_t pieceSize, left = 0, remainder = 0;
+
+	/* TODO: add argument checks */
+
+	offset = cache_compOffset(cache, addr);
+
+	left = count;
+
+	while (1) {
+		index = cache_compSet(cache, addr);
+		tag = cache_compTag(cache, addr);
+
+		if (left == count) {
+			if (count <= (cache->lineSize - offset)) {
+				pieceSize = count;
+			}
+			else {
+				pieceSize = cache->lineSize - offset;
+				remainder = (left - pieceSize) % cache->lineSize;
+			}
+			addr -= offset;
+		}
+		else if (left == remainder) {
+			pieceSize = remainder;
+		}
+		else {
+			pieceSize = cache->lineSize;
+		}
+
+		data = cache_readFromSet(&cache->sets[index], tag, offset);
+
+		/* cache miss */
+		if (data == NULL) {
+			position += cache_readOnMiss(cache, addr, offset, buffer, position, pieceSize);
+		}
+		else {
+			memcpy(buffer + position, data, pieceSize);
+			position += pieceSize;
+		}
+
+		left -= pieceSize;
+
+		if (left == 0) {
+			break;
+		}
+
+		addr += cache->lineSize;
 	}
-	/* cache hit */
-	// *data = *ret;
-	printf("data: %u\n", *data);
 
-	return wrt;
+	return position;
 }
 
 
-int cache_flush(cachectx_t *cache, const off_t begAddr, const uint64_t endAddr)
+int cache_flush(cachectx_t *cache, const uint64_t begAddr, const uint64_t endAddr)
 {
 }
 
 
-int cache_invalidate(cachectx_t *cache, const off_t begAddr, const uint64_t endAddr)
+int cache_invalidate(cachectx_t *cache, const uint64_t begAddr, const uint64_t endAddr)
 {
 }
 
@@ -340,20 +434,25 @@ int cache_invalidate(cachectx_t *cache, const off_t begAddr, const uint64_t endA
 #ifdef CACHE_DEBUG
 void cache_print(cachectx_t *cache)
 {
-	int i = 0, j = 0;
-	uint32_t data = 0;
+	int i, j;
+	unsigned char *data = NULL;
+	uint64_t offset;
 
+	printf("\n\n");
 	for (i = 0; i < cache->numSets; ++i) {
-		printf("set: %d\n", i);
+		printf("set: %2d\n", i);
+		printf("-------\n");
 		for (j = 0; j < LIBCACHE_NUM_WAYS; ++j) {
 			if (&cache->sets[i] != NULL) {
 				if (cache->sets[i].lines[j].data == NULL) {
-					data = 0;
+					offset = 0;
+					data = NULL;
 				}
 				else {
-					data = *(cache->sets[i].lines[j].data);
+					offset = cache->sets[i].lines[j].offset;
+					data = (unsigned char *)(cache->sets[i].lines[j].data + offset);
 				}
-				printf("line: %d\t\t%20lu\t%20u\n", j, cache->sets[i].lines[j].tag, data);
+				printf("line: %1d\t\t%20lu\t%5li\t%20s\n", j, cache->sets[i].lines[j].tag, offset, data);
 			}
 		}
 	}
--- libcache/cache.h
+++ libcache/cache.h
@@ -26,10 +26,10 @@
 typedef struct cachectx_s cachectx_t;
 
 
-typedef ssize_t (*cache_readCb_t)(off_t offset, void *buffer, size_t size, size_t count);
+typedef ssize_t (*cache_readCb_t)(uint64_t offset, void *buffer, size_t size, size_t count);
 
 
-typedef ssize_t (*cache_writeCb_t)(off_t offset, const void *buffer, size_t size, size_t count);
+typedef ssize_t (*cache_writeCb_t)(uint64_t offset, const void *buffer, size_t size, size_t count);
 
 
 cachectx_t *cache_init(size_t size, size_t lineSize, cache_writeCb_t writeCb, cache_readCb_t readCb);
@@ -38,16 +38,16 @@ cachectx_t *cache_init(size_t size, size_t lineSize, cache_writeCb_t writeCb, ca
 int cache_deinit(cachectx_t *cache);
 
 
-ssize_t cache_read(cachectx_t *cache, const off_t addr, void *buffer);
+ssize_t cache_read(cachectx_t *cache, uint64_t addr, void *buffer, size_t count);
 
 
-ssize_t cache_write(cachectx_t *cache, const off_t addr, void *buffer, int policy);
+ssize_t cache_write(cachectx_t *cache, const uint64_t addr, void *buffer, size_t count, int policy);
 
 
-int cache_flush(cachectx_t *cache, const off_t begAddr, const uint64_t endAddr);
+int cache_flush(cachectx_t *cache, const uint64_t begAddr, const uint64_t endAddr);
 
 
-int cache_invalidate(cachectx_t *cache, const off_t begAddr, const uint64_t endAddr);
+int cache_invalidate(cachectx_t *cache, const uint64_t begAddr, const uint64_t endAddr);
 
 
 #define CACHE_DEBUG
